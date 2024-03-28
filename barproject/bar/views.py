d9ec8d6815e1699678406228da4d734f86794f6b
NkongS <kong.songsri@gmail.com>
2024-03-29 06:19:15 +0700

    location

--- barproject/bar/views.py
+++ barproject/bar/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Locations
 
 # Create your views here.
+def locations(request):
+    locations = Locations.objects.all()
+    return render(request, 'locations.html'), {'locations': locations}
