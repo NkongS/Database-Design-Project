23f2eb4370a2db26efb682072c309270c8ef0dd8
NkongS <kong.songsri@gmail.com>
2024-03-31 05:57:06 +0700

    update query

--- Data_Query.sql
+++ Data_Query.sql
@@ -1,5 +1,10 @@
+--These query were translated from the django code to postgreSQL code so there might be some errors
+
 -- Get all FeedbackReviews
 SELECT * FROM Feedback_Reviews;
+
+-- Create a new FeedbackReview
+INSERT INTO Feedback_Reviews(membership_id, rating, feedbacks) VALUES (:membership_id, :rating, :feedbacks);
 
 -- Get a specific Branch by branch_id
 SELECT * FROM Branches WHERE branch_id = :branch_id;
@@ -19,6 +24,9 @@ INSERT INTO OrderProduct (order_id, product_name, quantity, branch_id) VALUES (:
 -- Get Orders for a specific branch and table
 SELECT * FROM Orders WHERE branch_id = :branch_id AND table_id = :table_id;
 
+-- Update an Order to mark it as completed
+UPDATE Orders SET completed = TRUE WHERE order_id = :order_id;
+
 -- Delete Orders for a specific table
 DELETE FROM Orders WHERE table_id = :table_id;
 
@@ -32,14 +40,34 @@ SELECT * FROM Reservations WHERE branch_id = :branch_id;
 -- Get all Memberships
 SELECT * FROM Membership;
 
--- Update an Order to mark it as completed
-UPDATE Orders SET completed = True WHERE order_id = :order_id;
-
 -- Create a new Reservation
-INSERT INTO Reservations (branch, table, membership_id, reservation_time, number_of_guests) VALUES (:branch, :table, :membership_id, :reservation_time, :number_of_guests);
+INSERT INTO Reservations (branch_id, table_id, membership_id, reservation_time, number_of_guests) VALUES (:branch, :table, :membership_id, :reservation_time, :number_of_guests);
 
 -- Update a Bartable to mark it as reserved
 UPDATE Bartables SET table_status = True, start_time = :reservation_time WHERE table_id = :table_id;
 
 -- Get Bartables for a specific branch that are not reserved ordered by table_id
 SELECT * FROM Bartables WHERE branch_id = :branch_id AND table_status = False ORDER BY table_id;
+
+INSERT INTO Membership (membership_id, first_name, second_name, contact_info, membership_status) VALUES (:membership_id, :first_name, :second_name, :contact_info, :membership_status);
+
+-- Trigger to update inventory's item quantity when an order is completed
+CREATE OR REPLACE FUNCTION update_inventory_after_order_completed() RETURNS TRIGGER AS $$
+DECLARE
+    order_items CURSOR FOR SELECT * FROM OrderProduct WHERE order_id = NEW.order_id;
+    item RECORD;
+    product_in_inventory RECORD;
+BEGIN
+    IF NEW.completed THEN
+        FOR item IN order_items LOOP
+            SELECT * INTO product_in_inventory FROM Bar_Inventory WHERE product_id = item.product_id;
+            UPDATE Bar_Inventory SET quantity = product_in_inventory.quantity - item.quantity WHERE product_id = item.product_id;
+        END LOOP;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_inventory_after_order_completed_trigger
+AFTER INSERT OR UPDATE ON Orders
+FOR EACH ROW EXECUTE PROCEDURE update_inventory_after_order_completed();
